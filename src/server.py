4f4aa3df1938d2319af76e73a6a7de4025eb12cd
Carl Edman <CarlEdman@gmail.com>
2026-01-21 06:10:32 -0500

    .

--- src/server.py
+++ src/server.py
@@ -4,8 +4,8 @@ import uvicorn
 import sqlmodel
 import asyncio
 
-from fastapi import FastAPI, WebSocket, Depends, HTTPException, Query, WebSocketDisconnect
-from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
+from fastapi import FastAPI, WebSocket, Depends, HTTPException, Query, Response, WebSocketDisconnect, status
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse, FileResponse
 from typing import Annotated
 from sqlmodel import Field, Session, SQLModel
 from contextlib import asynccontextmanager
@@ -15,8 +15,8 @@ from templates import templates
 from static import static
 
 def get_session():
-    with Session(engine) as session:
-        yield session
+  with Session(engine) as session:
+    yield session
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -30,19 +30,19 @@ engine = sqlmodel.create_engine(settings.sqlurl, connect_args={"check_same_threa
 app.mount("/static", static , name="static")
 
 @app.get("/favicon.ico")
-async def favicon() -> FileResponse:
+async def favicon() -> Response:
   return FileResponse( "static/favicon.ico")
 
 @app.get("/")
-async def root() -> FileResponse:
+async def root() -> Response:
   return FileResponse("static/index.html")
 
-# async def root() -> RedirectResponse:
-#   return RedirectResponse(url="static/index.html", status_code=307)
+# async def root() -> Response:
+#   return RedirectResponse(url="static/index.html", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-  return {"item_id": item_id}
+async def read_item(item_id: int) -> Response:
+  return JSONResponse({"item_id": item_id})
 
 websockets : set[WebSocket] = set()
 
